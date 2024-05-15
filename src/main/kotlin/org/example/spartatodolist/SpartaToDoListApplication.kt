335b101f07cf23ca5bf544b8bccf71559219402c
PHJUNITY <45454119+PHJUNITY@users.noreply.github.com>
2024-05-15 13:24:59 +0900

    처음 파일생성

---
+++ src/main/kotlin/org/example/spartatodolist/SpartaToDoListApplication.kt
@@ -0,0 +1,11 @@
+package org.example.spartatodolist
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpartaToDoListApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpartaToDoListApplication>(*args)
+}
---
+++ src/main/kotlin/org/example/spartatodolist/infra/swagger/SwaggerConfig.kt
@@ -0,0 +1,4 @@
+package org.example.spartatodolist.infra.swagger
+
+class SwaggerConfig {
+}
